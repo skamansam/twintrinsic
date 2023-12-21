4b06162473a31f27253bd102c77d120212b24201
skamansam <skamansam@gmail.com>
2023-12-21 14:05:38 +0000

    update App for darkmode

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,10 @@
 // Reexport your entry components here
 import Hero from "./Panels/Hero/Hero.svelte";
 import Section from "./Section.svelte";
-
+import App from "./App";
 
 export {
+  App,
   Section,
   Hero,
 };
