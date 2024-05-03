5fd60f762d5da9cf16a38d803b5ad792a4f4b390
Samuel C. Tyler <skamansam@gmail.com>
2024-05-03 19:09:08 -0400

    update from desktop

---
+++ src/lib/Menu/Menu/MenuItem.svelte
@@ -0,0 +1,22 @@
+<script>
+
+  export let open = false;
+  export let value = {};
+
+  const toggleMenu = () => {
+    open = !open;
+  };
+</script>
+
+<li>
+  { value.text }
+  {#if value?.children}
+    <ul>
+      {#each value.children as child}
+        <MenuItem {value:child}/>
+      {/each}
+    </ul>
+
+  {/if}
+
+</li>
