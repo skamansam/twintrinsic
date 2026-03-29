b1c655f5cefc2b617b53239043171f51c142c208
Samuel C Tyler <skamansam@gmail.com>
2026-03-29 12:38:40 -0400

    update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,20 @@
-import type { StorybookConfig } from "@storybook/sveltekit"
-import path from "path"
-import { fileURLToPath } from "url"
+import type { StorybookConfig } from "@storybook/sveltekit";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config: StorybookConfig = {
   stories: ["../stories/**/*.stories.@(js|ts|svelte|mdx)"],
 
-  addons: [
-    "@storybook/addon-svelte-csf",
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs"
-  ],
+  addons: ["@storybook/addon-svelte-csf", "@chromatic-com/storybook", "@storybook/addon-docs"],
 
   framework: {
     name: "@storybook/sveltekit",
     options: {},
   },
+
+  outDir: "../dist/storybook",
 
   viteFinal: (config) => {
     return {
@@ -24,13 +22,10 @@ const config: StorybookConfig = {
       server: {
         ...config.server,
         fs: {
-          allow: [
-            ...(config.server?.fs?.allow || []),
-            path.resolve(__dirname, "../stories")
-          ]
-        }
-      }
-    }
-  }
-}
-export default config
+          allow: [...(config.server?.fs?.allow || []), path.resolve(__dirname, "../stories")],
+        },
+      },
+    };
+  },
+};
+export default config;
