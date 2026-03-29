dc0bffed1a5f9196f77da57a703aca1d187682a8
Samuel C Tyler <skamansam@gmail.com>
2026-03-29 18:11:40 -0400

    try more

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,34 +1,39 @@
 import type { StorybookConfig } from "@storybook/sveltekit";
+import { defineMain } from "@storybook/sveltekit/node";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const config: StorybookConfig = {
-  stories: ["../stories/**/*.stories.@(js|ts|svelte|mdx)"],
-  addons: ["@storybook/addon-svelte-csf", "@chromatic-com/storybook", "@storybook/addon-docs"],
-  framework: {
-    name: "@storybook/sveltekit",
-    options: {},
-  },
-
+export default defineMain({
+  framework: "@storybook/sveltekit",
+  stories: [
+    // "../src/**/*.(stories|story).@(js|ts|svelte|mdx)",
+    "../stories/**/*.(stories|story).@(js|ts|svelte|mdx)",
+  ],
+  addons: [
+    "@storybook/addon-svelte-csf",
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-mcp",
+  ],
   async viteFinal(config, { configType }) {
     const { mergeConfig } = await import("vite");
     console.log("CONFIG TYPE", configType);
-    const merged = mergeConfig(config, {
-      base: configType === "PRODUCTION" ? "/storybook/" : "/",
+    const thisConfig: StorybookConfig = {
       server: {
-        path: "/storybook/",
         fs: {
           allow: [...(config.server?.fs?.allow || []), path.resolve(__dirname, "../stories")],
         },
       },
-    });
-
-    console.log("ViteFinal: ", merged);
-    return merged;
+    };
+    if (configType === "DEVELOPMENT") {
+      // Your development configuration goes here
+    }
+    if (configType === "PRODUCTION") {
+      // Your production configuration goes here.
+      thisConfig.base = "/storybook/";
+    }
+    return mergeConfig(config, thisConfig);
   },
-};
-console.log("CONFIG", config);
-
-export default config;
+});
